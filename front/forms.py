84383a9a0cb603e0a3b58093ba12eca299968aaf
kbr1009 <reo109r@icloud.com>
2021-01-03 00:40:07 +0900

    完成

--- front/forms.py
+++ front/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.contrib.auth.models import User
-from .models import List
+from .models import List, Card
 
 class UserForm(forms.ModelForm):
 
@@ -13,3 +13,20 @@ class ListForm(forms.ModelForm):
     class Meta:
         model = List
         fields = ("title",)
+
+class CardForm(forms.ModelForm):
+
+    class Meta:
+        model = Card
+        fields = ("title", "description", "list")
+
+class CardCreateFromHomeForm(forms.ModelForm):
+
+    def __init__(self, *args, **kwargs):
+        super(CardCreateFromHomeForm, self).__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
+
+    class Meta:
+        model = Card
+        fields = ("title", "description",)
--- front/views.py
+++ front/views.py
@@ -3,22 +3,26 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.models import User
-from django.shortcuts import render, redirect, resolve_url
+from django.shortcuts import render, redirect, resolve_url, get_object_or_404
 from django.urls import reverse_lazy
-from django.views.generic import DetailView, UpdateView, CreateView, ListView
+from django.views.generic import DetailView, UpdateView, CreateView, ListView, DeleteView
 
-from .forms import UserForm, ListForm
+from .forms import UserForm, ListForm, CardForm, CardCreateFromHomeForm
 from .mixins import OnlyYouMixin
-from .models import List
+from .models import List, Card
 
 def index(request):
     return render(request, "front/index.html")
 
 
+class HomeView(LoginRequiredMixin, ListView):
+    model = List
+    template_name = "front/home.html"
+"""
 @login_required # 追加
 def home(request):
     return render(request, "front/home.html")
-
+"""
 
 def signup(request):
     if request.method == 'POST':
@@ -67,3 +71,71 @@ class ListListView(LoginRequiredMixin, ListView):
 class ListDetailView(LoginRequiredMixin, DetailView):
     model = List
     template_name = "front/lists/detail.html"
+
+class ListUpdateView(LoginRequiredMixin, UpdateView):
+    model = List
+    template_name = "front/lists/update.html"
+    form_class = ListForm
+    success_url = reverse_lazy("front:home")
+
+    def get_success_url(self):
+        return resolve_url('front:lists_detail', pk=self.kwargs['pk'])
+
+class ListDeleteView(LoginRequiredMixin, DeleteView):
+    model = List
+    template_name = "front/lists/delete.html"
+    form_class = ListForm
+    success_url = reverse_lazy("front:home")
+
+
+
+class CardCreateView(LoginRequiredMixin, CreateView):
+    model = Card
+    template_name = "front/cards/create.html"
+    form_class = CardForm
+    success_url = reverse_lazy("front:home")
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+
+class CardListView(LoginRequiredMixin, ListView):
+    model = Card
+    template_name = "front/cards/list.html"
+
+
+class CardDetailView(LoginRequiredMixin, DetailView):
+    model = Card
+    template_name = "front/cards/detail.html"
+
+
+class CardUpdateView(LoginRequiredMixin, UpdateView):
+    model = Card
+    template_name = "front/cards/update.html"
+    form_class = CardForm
+    success_url = reverse_lazy("front:home")
+
+"""
+    def get_success_url(self):
+        return resolve_url('front:cards_detail', pk=self.kwargs['pk'])
+"""
+
+class CardDeleteView(LoginRequiredMixin, DeleteView):
+    model = Card
+    template_name = "front/cards/delete.html"
+    form_class = CardForm
+    success_url = reverse_lazy("front:home")
+
+
+class CardCreateFromHomeView(LoginRequiredMixin, CreateView):
+    model = Card
+    template_name = "front/cards/create.html" 
+    form_class = CardCreateFromHomeForm
+    success_url = reverse_lazy("front:home")
+
+    def form_valid(self, form):
+        list_pk = self.kwargs['list_pk']
+        list_instance = get_object_or_404(List, pk=list_pk)
+        form.instance.list = list_instance
+        form.instance.user = self.request.user
+        return super().form_valid(form)
