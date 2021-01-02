9033f8ecc118fcdeb2dd36aec4b48a8c67073cbe
kbr1009 <reo109r@icloud.com>
2021-01-03 02:57:58 +0900

    パッケージ管理

--- todoapp_config/urls.py
+++ todoapp_config/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path,include
 
 urlpatterns = [
-    path('front/' , include("front.urls")),
+    path('' , include("front.urls")),
     path('front/' , include('django.contrib.auth.urls')),
     path('admin/', admin.site.urls),
 ]
